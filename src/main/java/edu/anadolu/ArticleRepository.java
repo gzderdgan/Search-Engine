d3b7a1f78c264446ed1c9b3e3551282fa482993a
GÖZDE <gzd_erdgn_1994@hotmail.com>
2017-05-10 23:17:45 +0300

    commited

---
+++ src/main/java/edu/anadolu/ArticleRepository.java
@@ -0,0 +1,28 @@
+package edu.anadolu;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.solr.core.query.result.FacetPage;
+import org.springframework.data.solr.core.query.result.HighlightPage;
+import org.springframework.data.solr.core.query.result.SolrResultPage;
+import org.springframework.data.solr.repository.Facet;
+import org.springframework.data.solr.repository.Highlight;
+import org.springframework.data.solr.repository.Query;
+import org.springframework.data.solr.repository.SolrCrudRepository;
+
+
+public interface ArticleRepository extends SolrCrudRepository<Article, String> {
+
+    @Highlight(prefix = "<b>", postfix = "</b>", fields = {"title", "content"})
+    @Query(fields = {"title", "content"}, defaultOperator = org.springframework.data.solr.core.query.Query.Operator.OR)
+    HighlightPage<Article> findByContent(String content, Pageable page);
+
+    @Facet(fields = {"source"}, limit = 10)
+    @Highlight(prefix = "<b>", postfix = "</b>", fields = {"title", "content"})
+    SolrResultPage<Article> findByTitle(String title, Pageable page);
+
+    //Execute faceted search
+    //Query will be "q=content:<name>&facet=true&facet.field=source&facet.limit=20&start=<page.number>&rows=<page.size>"
+    @Query(value = "name:?0")
+    @Facet(fields = {"source"}, limit = 20)
+    FacetPage<Article> findByContentAndFacetOnSource(String title, Pageable page);
+}
